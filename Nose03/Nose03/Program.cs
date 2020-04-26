ff3a010e24b57be44e728bc34c3d3d763b46c8f2
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-04-26 16:11:24 +0200

    rgrt

---
+++ Nose03/Nose03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nose03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
