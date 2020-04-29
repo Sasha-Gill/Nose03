048c6cb2b3433d358a515e66f8f0d145822e57f1
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2020-04-29 15:05:03 +0200

    2 uppgifter klart

--- Nose03/Nose03/Program.cs
+++ Nose03/Nose03/Program.cs
@@ -4,9 +4,26 @@ namespace Nose03
 {
     class Program
     {
+    static void hello32()//Metoden döps till Hello32 och är en static void. Static för att den är i Main och void för att den inte returnerar nått
+        {
+            for (int i = 0; i < 32; i++)//for loop som kör som 32 ggr
+            {
+                Console.WriteLine("Hello "+i);//lade till i för att hålla räkning på hur många gånger.
+                Console.ReadLine();
+            }
+        }
+    static int Square(int x)//skapar en int metod med parametern int. Värdet är X då man själv kan välja värdet.
+        {
+            return x * x;//returnerar värdet gånger sig själv.
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            hello32();//kallade på metoden jag nyss skapat
+
+            Square(4);//kallar på denna metod.  
         }
-    }
+       
+
+}
 }
